9deae2be9ed4ab72c48c5ba26eff535920177317
huyhungikame <115602357+huyhungikame@users.noreply.github.com>
2024-05-15 11:51:53 +0700

    Use UXML

--- Editor/GameFlowManagerEditorWindow.cs
+++ Editor/GameFlowManagerEditorWindow.cs
@@ -4,6 +4,8 @@ using UnityEngine.UIElements;
 
 public class GameFlowManagerEditorWindow : EditorWindow
 {
+    private const string UXML_PATH = "Packages/com.huyhung1404.gameflow/Editor/UXML/GameFlowManagerEditor.uxml";
+
     public static void OpenWindow()
     {
         var window = GetWindow<GameFlowManagerEditorWindow>();
@@ -12,23 +14,8 @@ public class GameFlowManagerEditorWindow : EditorWindow
 
     public void CreateGUI()
     {
-        // Each editor window contains a root VisualElement object
-        VisualElement root = rootVisualElement;
-
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
-
-        // Import UXML
-        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/com.huyhung1404.gameflow/Editor/UXML/GameFlowManagerEditor.uxml");
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(UXML_PATH);
         VisualElement labelFromUXML = visualTree.Instantiate();
-        root.Add(labelFromUXML);
-
-        // A stylesheet can be added to a VisualElement.
-        // The style will be applied to the VisualElement and all of its children.
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Packages/com.huyhung1404.gameflow/Editor/USS/GameFlowManagerEditor.uss");
-        VisualElement labelWithStyle = new Label("Hello World! With Style");
-        labelWithStyle.styleSheets.Add(styleSheet);
-        root.Add(labelWithStyle);
+        rootVisualElement.Add(labelFromUXML);
     }
 }
