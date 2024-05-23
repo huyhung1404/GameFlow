42f7d9d7d32af0e2ee1874c36b59b35a069ff883
huyhung1404 <115602357+huyhungikame@users.noreply.github.com>
2024-05-23 21:32:08 +0700

    Update GameFlowElement.cs

--- Runtime/Elements/GameFlowElement.cs
+++ Runtime/Elements/GameFlowElement.cs
@@ -7,6 +7,7 @@ namespace GameFlow
     [Serializable]
     public class GameFlowElement
     {
+        [SerializeField] internal string instanceID;
         [SerializeField] internal bool includeInBuild = true;
         [SerializeField] internal AssetReference reference;
         [SerializeField] internal ElementReleaseMode releaseMode;
