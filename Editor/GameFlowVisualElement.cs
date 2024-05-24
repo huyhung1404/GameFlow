10e87439fa518a579fcac505f2052f9f35d5b0f6
huyhungikame <115602357+huyhungikame@users.noreply.github.com>
2024-05-24 17:07:04 +0700

    Editor Visual

---
+++ Editor/GameFlowVisualElement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace GameFlow.Editor
+{
+    public class GameFlowVisualElement : VisualElement
+    {
+        private const string kUxmlPath = "Packages/com.huyhung1404.gameflow/Editor/UXML/GameFlowVisualElement.uxml";
+
+        public GameFlowVisualElement()
+        {
+            var root = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(kUxmlPath).CloneTree();
+            Add(root);
+        }
+
+        public new class UxmlFactory : UxmlFactory<GameFlowVisualElement, UxmlTraits>
+        {
+        }
+
+        public new class UxmlTraits : VisualElement.UxmlTraits
+        {
+            public override IEnumerable<UxmlChildElementDescription> uxmlChildElementsDescription { get { yield break; } }
+        }
+    }
+}
