bf02fad11c6499730b8eec68ebd5cdc1362c4fd7
huyhung1404 <115602357+huyhungikame@users.noreply.github.com>
2024-05-27 19:18:47 +0700

    Asset Reference Element

---
+++ Runtime/Elements/AssetReferenceElement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using Object = UnityEngine.Object;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace GameFlow
+{
+    [Serializable]
+    public class AssetReferenceElement : AssetReferenceT<Object>
+    {
+        public AssetReferenceElement(string guid) : base(guid)
+        {
+        }
+
+        public override bool ValidateAsset(Object obj)
+        {
+#if UNITY_EDITOR
+            return obj is GameObject || obj is SceneAsset;
+#else
+            return true;
+#endif
+        }
+
+        public override bool ValidateAsset(string path)
+        {
+#if UNITY_EDITOR
+            var type = AssetDatabase.GetMainAssetTypeAtPath(path);
+            return type == typeof(GameObject) || type == typeof(SceneAsset);
+#else
+            return false;
+#endif
+        }
+    }
+}
