c2c59cd20ad350d63fd8f2abb843449479d7a7ab
huyhung1404 <73883857+huyhung1404@users.noreply.github.com>
2024-06-09 15:57:37 +0700

    Update FlowEventListener.cs

--- Runtime/Components/FlowEventListener.cs
+++ Runtime/Components/FlowEventListener.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +6,31 @@ namespace GameFlow.Component
 {
     public class FlowEventListener : MonoBehaviour
     {
-        
+        public enum EventTriggerType
+        {
+        }
+
+        [Serializable]
+        public abstract class Entry
+        {
+            public EventTriggerType eventId;
+            public abstract void Register(Type type);
+            public abstract void Unregister(Type type);
+        }
+
+        [SerializeField] private GameFlowElement element;
+        [SerializeReference] private List<Entry> delegates;
+
+        private void OnEnable()
+        {
+            var type = element.elementType;
+            for (var i = delegates.Count - 1; i >= 0; i--) delegates[i].Register(type);
+        }
+
+        private void OnDisable()
+        {
+            var type = element.elementType;
+            for (var i = delegates.Count - 1; i >= 0; i--) delegates[i].Unregister(type);
+        }
     }
 }
