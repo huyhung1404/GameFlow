59f13f46f1cb9c39f238b2c1d2143187430e4612
huyhungikame <HUNGNH@iKameglobal.com>
2024-06-11 14:14:33 +0700

    Change Test Folder

---
+++ Tests/Runtime/Builder/PrebuildSetup.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace GameFlow.Tests.Build
+{
+    public static class PrebuildSetup
+    {
+        public static void Checking()
+        {
+        }
+    }
+}
