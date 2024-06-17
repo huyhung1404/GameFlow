8f0a65a6aa36b654e0879bd111e352999789f40f
huyhungikame <HUNGNH@iKameglobal.com>
2024-06-17 17:19:42 +0700

    Simple UI Animation

---
+++ Runtime/Components/SimpleUIAnimation.cs
@@ -0,0 +1,17 @@
+using GameFlow.Component;
+using UnityEngine;
+
+namespace GameFlow
+{
+    [AddComponentMenu("Game Flow/UI Simple Animation")]
+    public class SimpleUIAnimation : GameFlowUIAnimation
+    {
+        protected override void OnShow()
+        {
+        }
+
+        protected override void OnHide()
+        {
+        }
+    }
+}
