19912962f05248079e292b4a818bb9aa161f0d6e
huyhungikame <HUNGNH@iKameglobal.com>
2024-07-03 11:26:18 +0700

    UI Canvas With Animation

---
+++ Runtime/Components/GameFlowUICanvasWithAnimation.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameFlow.Component
+{
+    [RequireComponent(typeof(Canvas))]
+    [RequireComponent(typeof(CanvasScaler))]
+    [AddComponentMenu("Game Flow/UI Canvas With Animation")]
+    public abstract class GameFlowUICanvasWithAnimation : GameFlowUICanvas
+    {
+        private ICommandReleaseHandle releaseHandle;
+
+        protected override void RegisterDelegates(UIElementCallbackEvent delegates)
+        {
+            base.RegisterDelegates(delegates);
+            delegates.OnActive += OnShow;
+            delegates.OnHide += OnHide;
+        }
+
+        protected override void UnregisterDelegates(UIElementCallbackEvent delegates)
+        {
+            base.UnregisterDelegates(delegates);
+            delegates.OnActive -= OnShow;
+            delegates.OnHide -= OnHide;
+        }
+
+        private void OnHide(ICommandReleaseHandle handle)
+        {
+            releaseHandle = handle;
+            OnHide();
+        }
+
+        protected abstract void OnShow();
+        protected abstract void OnHide();
+
+        protected void OnShowCompleted()
+        {
+            FlowObservable.UIEvent(element.elementType).RaiseOnShowCompleted();
+        }
+
+        protected void OnHideCompleted()
+        {
+            if (releaseHandle == null) return;
+            releaseHandle.Next();
+            releaseHandle = null;
+        }
+    }
+}
